30d33abf3458f8f4740e06f1b51dd42d36ef92aa
Kevin <kamcak@hotmail.es>
2022-12-13 16:27:03 -0500

    Context Created

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,10 +1,18 @@
-function TaskCard({task, deleteTask}) {
+import { useContext } from "react";
+import {TaskContext} from "../context/TaskContext";
+
+
+
+function TaskCard({ task, deleteTask }) {
+  const valor = useContext(TaskContext)
+  console.log(valor)
+
   return (
-    <div>
-      <h1>{task.title}</h1>
-      <p>{task.description}</p>
-      <button onClick={() => deleteTask(task.id)}>Eliminar</button>
-    </div>
+      <div>
+        <h1>{task.title}</h1>
+        <p>{task.description}</p>
+        <button onClick={() => deleteTask(task.id)}>Eliminar</button>
+      </div>
   );
 }
 
